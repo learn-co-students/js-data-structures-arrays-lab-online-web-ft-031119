997c1bf422b63305cdc90e863f28b1002cf28e07
Annie Phan <anniephan.atp@gmail.com>
2019-06-18 23:25:41 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name) {
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name) {
+  drivers.unshift(name);
+}
+
+function destructivelyRemoveLastDriver() {
+  drivers.pop();
+}
+
+function destructivelyRemoveFirstDriver() {
+  drivers.shift();
+}
+
+function appendDriver(name) {
+  const copy_of_drivers = drivers.slice();
+  copy_of_drivers.push(name);
+  return copy_of_drivers;
+}
+
+function prependDriver(name) {
+  const copy_of_drivers = drivers.slice();
+  copy_of_drivers.unshift(name);
+  return copy_of_drivers;
+}
+
+function removeLastDriver() {
+  const copy_of_drivers = drivers.slice();
+  copy_of_drivers.pop();
+  return copy_of_drivers;
+}
+
+function removeFirstDriver() {
+  const copy_of_drivers = drivers.slice();
+  copy_of_drivers.shift();
+  return copy_of_drivers;
+}
